2b196e1a23d22cb3b9d600f553e7b33207f55068
Carolyn Ronchinsky <cronchinsky@edc.org>
2013-10-17 21:41:23 -0400

    quiz updates and some css and image changes

---
+++ modules/custom/ezsteps_certificate/templates/ezsteps_journal.tpl.php
@@ -0,0 +1,12 @@
+<div id="ezsteps-journal-wrapper">
+  <h2>Module <?php print $module_num ?>: <?php print $module_title ?></h2>
+  <h3>Journal Responses</h3>
+  <hr />
+  <?php print $question_a; print $response_a; ?>
+   <hr />
+   <?php print $question_b; print $response_b; ?>
+   <hr />
+   <?php print $radio_yes; ?>
+   <hr />
+   <?php print $radio_np; ?>
+</div>
--- modules/custom/ezsteps_module_viewer/js/viewer-brightcove.js
+++ modules/custom/ezsteps_module_viewer/js/viewer-brightcove.js
@@ -3,19 +3,38 @@
     attach: function () {
          
       // Hopefully we will add Brightcove API code to stop and start the player
-         
+             // Brightcove Learning namespace
+/*
+		var BCLS = {};
+		console.log('bsls script');
+		//template loaded event handler
+		BCLS.onTemplateLoad = function (experienceID) {
+		  console.log("template loaded");
+		  // get references to the player and API Modules and Events
+		  BCLS.player = brightcove.api.getExperience(experienceID);
+		  BCLS.APIModules = brightcove.api.modules.APIModules;
+		};
+		     
+		    
+		BCLS.onTemplateReady = function (evt) {
+		  console.log("template ready");
+		  BCLS.videoPlayer = BCLS.player.getModule(BCLS.APIModules.VIDEO_PLAYER);
+		};
+*/
+		
       // Opens the learn tab to start.
       $('.horizontal-tab-button.last a').click();
       
       // When you click the try tab, if the video is playing, pause it.
       $('.horizontal-tab-button.first a').click(function () {
-        //var playerId = $('.field-name-field-activity-video .jwplayer').attr('id');
-        //var player = jwplayer(playerId);
-        /*
-	    if (player.getIsPlaying()) {
-          player.pause();
 
-        } */
+/*
+        var isPlaying = BCLS.videoPlayer.getIsPlaying();
+        if (isPlaying) {
+	        BCLS.videoPlayer.pause();
+        }
+*/
+        
       });
       
       // return user to the try tab when closing an interactive
