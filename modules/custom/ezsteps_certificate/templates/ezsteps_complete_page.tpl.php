95fdd698cb69437ff30c71f23b43e3191cd0346b
Carolyn Ronchinsky <cronchinsky@edc.org>
2013-11-22 19:23:44 -0500

    last round of major updates

--- modules/custom/ezsteps_certificate/templates/ezsteps_complete_page.tpl.php
+++ modules/custom/ezsteps_certificate/templates/ezsteps_complete_page.tpl.php
@@ -1,9 +1,10 @@
 <div id="ezsteps-complete-page-wrapper">
-  <h3><?php print $complete_header; ?>!</h3>
+  <h3><?php print $complete_header; ?></h3>
   <div id="ezsteps-complete-page-content">
     <div id="ezsteps-complete-page-actions">
       <a href="/journal/<?php print $module_num; ?>" class="ezsteps-complete-page-certificate-link"><span class="less-than">&lt;</span> <span><?php print $download_text ?></span></a>
      <!-- <a href="/module-list" class="ezsteps-complete-page-modules-link">Return to the Module List</a> -->
     </div>
+    <p><?php print $complete_text ?></p>
   </div>
 </div>
