a148bd5b2fdde258e9eabf94fee2dfaee1e582c5
Carolyn Ronchinsky <cronchinsky@edc.org>
2013-11-24 22:11:39 -0500

    some clean up of brightcove js

--- themes/ezsteps/templates/field--field-activity-video-hosted.tpl.php
+++ themes/ezsteps/templates/field--field-activity-video-hosted.tpl.php
@@ -46,13 +46,13 @@
 
    	    var onTemplateLoad, onTemplateReady, player, videoPlayer, currPos;
 		var APIModules, mediaEvent, contentEvent;
-	    console.log('loaded brigthcove js');
+	    //console.log('loaded brigthcove js');
 	    onTemplateLoad = function(experienceID) {
 	        player = brightcove.api.getExperience(experienceID);
 	        videoPlayer = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);	
 	        APIModules = brightcove.api.modules.APIModules;
 	        mediaEvent = brightcove.api.events.MediaEvent;
-	        console.log('template loaded');
+	        //console.log('template loaded');
 	    }
 		
 	    onTemplateReady = function(evt) {
@@ -64,7 +64,7 @@
 	        videoPlayer.addEventListener(brightcove.api.events.MediaEvent.COMPLETE, onMediaEventFired);
 	        videoPlayer.addEventListener(brightcove.api.events.MediaEvent.PLAY, onMediaEventFired);
 	        videoPlayer.addEventListener(brightcove.api.events.MediaEvent.PROGRESS, onMediaEventFired);
-	        console.log('template ready');
+	        //console.log('template ready');
 	    }
 	    
 		
