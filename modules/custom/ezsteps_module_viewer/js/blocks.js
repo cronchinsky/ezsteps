b9d069eaded51f8bb7824b2f0342aea473832709
cronchinsky <cronchinsky@edc.org>
2014-07-11 22:33:28 -0400

    I kept clicking the Mobile Connections header to close, so I made that work

--- modules/custom/ezsteps_module_viewer/js/blocks.js
+++ modules/custom/ezsteps_module_viewer/js/blocks.js
@@ -5,7 +5,7 @@
      var closeLink = $('<a href="#" id="ezsteps-mobile-close"> Close </a>');
      $('.field-name-field-activity-mobile p').append(closeLink);
      $('.block-ezsteps-module-viewer-mobile h2').click(function () {
-        $('.field-name-field-activity-mobile').show();
+        $('.field-name-field-activity-mobile').toggle();
      });
      $('#ezsteps-mobile-close').click(function () {
         $('.field-name-field-activity-mobile').hide();
